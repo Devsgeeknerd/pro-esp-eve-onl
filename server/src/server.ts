f35f831123bc4b26ea9c5c152cf4101525447395
Paulo Vanderley <devsgeeknerd@icloud.com>
2025-02-05 11:59:31 -0300

    :hammer_and_wrench: chore(deps): adiciona dependencias.

---
+++ server/src/server.ts
@@ -0,0 +1,8 @@
+// importa o módulo express para criar um servidor web.
+import express from "express";
+
+// Importa o módulo cors para permitir requisições de diferentes origens.
+import cors from "cors";
+
+// Importa o PrismaClient para interagir com o banco de dados através do Prisma ORM.
+import {PrismaClient} from "@prisma/client";
