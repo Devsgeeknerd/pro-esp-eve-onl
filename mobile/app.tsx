361d04634ceec197d48ad9163dfd8ab796603b36
Paulo Vanderley <devsgeeknerd@icloud.com>
2025-02-15 13:22:27 -0300

    :heavy_plus_sign: feat(app.tsx): adiciona arquivo principal do aplicativo mobile com imports de hooks do React.

---
+++ mobile/app.tsx
@@ -0,0 +1,5 @@
+// Importa os hooks e módulos necessários do React e outras bibliotecas.
+// useEffect para gerenciar efeitos colaterais e useRef para persistir valores entre renderizações.
+import {useEffect, useRef} from "react";
+
+
