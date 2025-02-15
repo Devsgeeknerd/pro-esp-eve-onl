c803c77559624e49549d93ddd6077a18a69c1384
Paulo Vanderley <devsgeeknerd@icloud.com>
2025-02-15 13:31:04 -0300

    :lady_beetle: fix(app.tsx): ajusta formatação de imports e adiciona importação do StatusBar.

--- mobile/app.tsx
+++ mobile/app.tsx
@@ -1,5 +1,6 @@
 // Importa os hooks e módulos necessários do React e outras bibliotecas.
 // useEffect para gerenciar efeitos colaterais e useRef para persistir valores entre renderizações.
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 
-
+// StatusBar para controlar a barra de status do dispositivo.
+import { StatusBar } from "@expo-google-fonts/inter";
